822f6ad7b98d5fd801d2ded788f2a7c0f07eb90f
Ngabgna <Ngabgna@BLACKJUKER>
2023-08-04 11:55:52 +0200

    work flow establishement15

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React JS 2
+          Learn React JS 2Z
         </a>
       </header>
     </div>
